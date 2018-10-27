c4e1aa9b1418dd78a58be1ac1ec0ba252e887154
DekuKnight <rdwright5127@gmail.com>
2018-10-26 20:27:29 -0400

    Trying to test podcast site
    just a test

---
+++ precache-manifest.9d97cb8e7121d0aeb14ef0e2ff1156c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6b8bd3520e5b0f9f564",
+    "url": "/static/js/main.f6b8bd35.chunk.js"
+  },
+  {
+    "revision": "f3528184afcd08288d3a",
+    "url": "/static/js/1.f3528184.chunk.js"
+  },
+  {
+    "revision": "f6b8bd3520e5b0f9f564",
+    "url": "/static/css/main.9fd59eea.chunk.css"
+  },
+  {
+    "revision": "d3923720b56584c5d27fbea7db8400b7",
+    "url": "/index.html"
+  }
+];
